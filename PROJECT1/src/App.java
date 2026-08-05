a6742d850945d02d5a68164c2bb7177c9378a71f
carolinafoliveira <coliveiracl.co@gmail.com>
2026-08-05 19:47:13 -0300

    Aula 05/08/2026 - Introdução ao Java

---
+++ PROJECT1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number1 = 5;
+        int number2 = 2;
+        int sum = number1 + number2;
+        System.out.println("Soma = " + sum);
+    }
+}
