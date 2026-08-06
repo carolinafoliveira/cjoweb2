dc07c5e1ebfba6f7046a2f64db0791d516d7aea6
Carolina Oliveira <127605618+carolinafoliveira@users.noreply.github.com>
2026-08-06 09:27:14 -0300

    Organização das pastas e resolução de exercicios de fixação da aula do dia 05/08/2026

---
+++ PROJECT1/src/exercicios_conteudo2/ex01.java
@@ -0,0 +1,25 @@
+
+/*Adriana comprou um saco de ração com peso em quilos. Ela possui dois gatos, para os quais fornece a quantidade de ração em gramas. 
+A quantidade diária de ração fornecida para cada gato é sempre a mesma. Faça um programa em Java que receba o peso do saco de ração e
+a quantidade de ração fornecida para cada gato, calcule e mostre quanto restará de ração no saco após cinco dias.*/
+
+package exercicios_conteudo2; // package organiza o projeto em pastas
+
+import javax.swing.JOptionPane;
+
+public class ex01 {
+    public static void main(String[] args) throws Exception {
+        float pesoSaco, racao, pesoGr, consumoTotal, resultado;
+
+        pesoSaco = Float.parseFloat(JOptionPane.showInputDialog("Digite o peso do saco (Kg):"));
+        racao = Float.parseFloat(JOptionPane.showInputDialog("Digite a quantidade de ração fornecida para os gatos (Gr):"));
+
+        pesoGr = pesoSaco * 1000; //conversão de kg para gramas
+        consumoTotal = racao * 2 * 5; // dois gatos em 5 dias
+        resultado = pesoGr - consumoTotal;
+
+        JOptionPane.showMessageDialog(null, "Consumo dos gatos em 5 dias = " + resultado);
+    }
+}
+
+
---
+++ PROJECT1/src/exercicios_conteudo2/ex02.java
@@ -0,0 +1,22 @@
+
+/*Faça um programa em Java que receba o custo de um espetáculo teatral e o preço do convite desse espetáculo. 
+Esse programa deverá calcular e mostrar a quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado*/
+
+package exercicios_conteudo2; // package organiza o projeto em pastas
+
+import javax.swing.JOptionPane;
+
+public class ex02 {
+    public static void main(String[] args) throws Exception {
+        float custoPeca, precoConvite, quantidadeConvite;
+
+        custoPeca = Float.parseFloat(JOptionPane.showInputDialog("Digite o custo total do espetáculo teatral (R$):"));
+        precoConvite = Float.parseFloat(JOptionPane.showInputDialog("Digite o preço do convite individual (R$):"));
+
+        quantidadeConvite = custoPeca / precoConvite; //ponto de equilibrio contábil = custo total / margem de contribuiçao
+
+        JOptionPane.showMessageDialog(null, "Venda minima deve ser de " + quantidadeConvite + " convites");
+    }
+}
+
+
