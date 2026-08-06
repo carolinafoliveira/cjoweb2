f645befd8a532a2b4d0f51abc86734ec31554bf1
Carolina Oliveira <127605618+carolinafoliveira@users.noreply.github.com>
2026-08-06 17:33:04 -0300

    Continuação exercicios conteudo 02

---
+++ PROJECT1/src/exercicios_conteudo2/ex03.java
@@ -0,0 +1,25 @@
+
+/*Um hotel deseja fazer uma promoção especial de final de semana, concedendo um desconto no 
+valor normal da diária. Faça um programa em Java que recebe o valor da diária e o percentual de 
+desconto que será concedido, calcule e mostre o valor da diária com desconto. */
+
+package exercicios_conteudo2; // package organiza o projeto em pastas
+
+import javax.swing.JOptionPane;
+
+public class ex03 {
+    public static void main(String[] args) throws Exception {
+
+        float precoDiaria, descontoDiaria, precoFinal, percentual;
+
+        precoDiaria = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor da diaria:"));
+        percentual = Float.parseFloat(JOptionPane.showInputDialog("Informe o % de desconto:"));
+
+        descontoDiaria = precoDiaria * (percentual / 100);
+        precoFinal = precoDiaria - descontoDiaria;
+
+        JOptionPane.showMessageDialog(null, "Valor final da Diaria" + precoFinal);
+    }
+}
+
+
---
+++ PROJECT1/src/exercicios_conteudo2/ex04.java
@@ -0,0 +1,15 @@
+
+/*Uma loja de informática está vendendo seus produtos em 5 (cinco) prestações sem juros. 
+Faça um programa em Java que recebe o valor de uma compra, calcule e mostre o valor das prestações.*/
+
+package exercicios_conteudo2; // package organiza o projeto em pastas
+
+import javax.swing.JOptionPane;
+
+public class ex04 {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
+
